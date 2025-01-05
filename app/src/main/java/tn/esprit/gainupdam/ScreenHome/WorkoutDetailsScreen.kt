9642fc0ad37eef167bff3757375ef9ea85d73ba2
Mohamed Borgi <mohamed.borgi@esprit.tn>
2025-01-05 20:26:33 +0100

    ne9esss

--- app/src/main/java/tn/esprit/gainupdam/ScreenHome/WorkoutDetailsScreen.kt
+++ app/src/main/java/tn/esprit/gainupdam/ScreenHome/WorkoutDetailsScreen.kt
@@ -1,7 +1,6 @@
 package tn.esprit.gainupdam.ScreenHome
 
 import android.os.Build
-import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
@@ -29,38 +28,26 @@ import tn.esprit.gainupdam.RetrofitInstance
 @Composable
 fun WorkoutDetailsScreen(navController: NavHostController, id: String) {
     var exercise by remember { mutableStateOf<Exercise?>(null) }
-    var error by remember { mutableStateOf<String?>(null) }
-    var isLoading by remember { mutableStateOf(true) }
 
     LaunchedEffect(id) {
         RetrofitInstance.api.getExerciseById(id).enqueue(object : Callback<Exercise> {
             override fun onResponse(call: Call<Exercise>, response: Response<Exercise>) {
-                isLoading = false
                 if (response.isSuccessful) {
                     exercise = response.body()
-                    Log.d("WorkoutDetailsScreen", "Exercise loaded: $exercise")
-                } else {
-                    error = "Failed to load exercise details"
-                    Log.e("WorkoutDetailsScreen", "Failed to load exercise details")
                 }
             }
 
             override fun onFailure(call: Call<Exercise>, t: Throwable) {
-                isLoading = false
-                error = "Error: ${t.message}"
-                Log.e("WorkoutDetailsScreen", "Error loading exercise: ${t.message}")
+                // Handle failure
             }
         })
     }
 
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = Color(0xFF03224c)
-    ) {
-        if (isLoading) {
-            // Afficher un indicateur de chargement
-            CircularProgressIndicator(color = Color.White, modifier = Modifier.size(50.dp))
-        } else {
+    exercise?.let {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = Color(0xFF03224c)
+        ) {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
@@ -91,57 +78,45 @@ fun WorkoutDetailsScreen(navController: NavHostController, id: String) {
                 }
                 Spacer(modifier = Modifier.height(16.dp))
 
-                // Error message
-                error?.let {
-                    Text(
-                        text = it,
-                        color = Color.Red,
-                        fontSize = 16.sp
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                }
+                // Image
+                Image(
+                    painter = rememberImagePainter(data = it.imageUrl),
+                    contentDescription = it.name,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(16.dp))
 
-                // Exercise details
-                exercise?.let {
-                    Image(
-                        painter = rememberImagePainter(data = it.imageUrl),
-                        contentDescription = it.name,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(200.dp),
-                        contentScale = ContentScale.Crop
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
+                // Workout title
+                Text(
+                    text = it.name,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 24.sp
+                )
+                Spacer(modifier = Modifier.height(16.dp))
 
-                    // Workout title
-                    Text(
-                        text = it.name,
-                        color = Color.White,
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 24.sp
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
+                // Workout description
+                Text(
+                    text = it.description,
+                    color = Color.White,
+                    fontSize = 16.sp
+                )
+                Spacer(modifier = Modifier.height(16.dp))
 
-                    // Workout description
-                    Text(
-                        text = it.description,
-                        color = Color.White,
-                        fontSize = 16.sp
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-
-                    // Workout details
-                    Text(
-                        text = "Duration: ${it.duration} min",
-                        color = Color.White,
-                        fontSize = 16.sp
-                    )
-                    Text(
-                        text = "Calories: ${it.calories} cal",
-                        color = Color.White,
-                        fontSize = 16.sp
-                    )
-                }
+                // Workout details
+                Text(
+                    text = "Duration: ${it.duration} min",
+                    color = Color.White,
+                    fontSize = 16.sp
+                )
+                Text(
+                    text = "Calories: ${it.calories} cal",
+                    color = Color.White,
+                    fontSize = 16.sp
+                )
                 Spacer(modifier = Modifier.weight(1f))
 
                 // Bottom navigation
